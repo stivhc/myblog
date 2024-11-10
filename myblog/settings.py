54ecd9ac19b86ed08c6214b40f0450e6661a9148
stiv.hc <esteban.alfonso999@gmail.com>
2024-11-10 19:36:03 -0300

    allowed host

--- myblog/settings.py
+++ myblog/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOST", "localhost").split(",")]
 
 
 # Application definition
